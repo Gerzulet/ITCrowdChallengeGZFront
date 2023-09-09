656cb927b32cfa30636574b7b30a4a66cf7a1013
German <german.alejandrozulet@gmail.com>
2023-09-09 02:00:39 -0300

    Added on hover animations to products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,63 +3,63 @@ const Products = () => {
 
     <div className="text-[24rem] overflow-y-scroll h-[50rem] md:w-[58rem] ml-48 md:ml-0 mt-10 md:mt-2">
       <div className="grid grid-cols-3 gap-2">
-        <div className="col-span-3 md:col-span-1 md:row-span-2">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300 col-span-3 md:col-span-1 md:row-span-2">
           <img
             className="h-auto"
             src="https://img.freepik.com/foto-gratis/vista-posterior-sudadera-capucha-ropa-hombres_53876-97228.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-1 md:row-span-2">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300  col-span-3 md:col-span-1 md:row-span-2">
           <img
             className="h-48"
             src="https://img.freepik.com/foto-gratis/concepto-maqueta-camisa-ropa-sencilla_23-2149448792.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-1">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300  col-span-3 md:col-span-1">
           <img
             className="h-28"
             src="https://img.freepik.com/foto-gratis/hombre-sueter-negro-sombrero-cubo-negro-sesion-ropa-juvenil_53876-102294.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-1">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300 col-span-3 md:col-span-1">
           <img
             className="h-54"
             src="https://img.freepik.com/foto-gratis/hermosa-mujer-gorra-beisbol-sesion-estudio-moda-diadema_53876-102175.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-1">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300 col-span-3 md:col-span-1">
           <img
             className="h-32"
             src="https://img.freepik.com/foto-gratis/vista-posterior-sudadera-capucha-ropa-hombres_53876-97228.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-2">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300 col-span-3 md:col-span-2">
           <img
             className="h-62"
             src="https://img.freepik.com/psd-premium/mockup-camiseta-hombre-psd-ropa-logo-verano_53876-141828.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-1">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300 col-span-3 md:col-span-1">
           <img
             className="h-48"
             src="https://img.freepik.com/foto-gratis/vista-posterior-sudadera-capucha-ropa-hombres_53876-97228.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-2">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300 col-span-3 md:col-span-2">
           <img
             className="h-92"
             src="https://img.freepik.com/foto-gratis/hombre-afroamericano-camiseta-negra-al-aire-libre_53876-97162.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph"
             alt=""
           />
         </div>
-        <div className="col-span-3 md:col-span-1">
+        <div className="transform p-4 hover:scale-110 transition-transform ease-in-out duration-300 col-span-3 md:col-span-1">
           <img
             className="h-60"
             src="https://img.freepik.com/psd-premium/maqueta-camiseta-negra-hombre-psd-modelo-tatuado_53876-150796.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph"
