c42369ffcd63d5ad56d2c6ffb43dd599ae7c09af
German <german.alejandrozulet@gmail.com>
2023-09-10 17:25:00 -0300

    creating mockup data, about to make it with fakerjs

--- Backend/prisma/seed.ts
+++ Backend/prisma/seed.ts
@@ -3,23 +3,67 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // Crear un producto con una marca relacionada
-  const product = await prisma.product.create({
+
+  const mockupData = [ // todo exportar esto de otra parte
+    'https://img.freepik.com/foto-gratis/vista-posterior-sudadera-capucha-ropa-hombres_53876-97228.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/vista-posterior-sudadera-capucha-ropa-hombres_53876-97228.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/concepto-maqueta-camisa-ropa-sencilla_23-2149448792.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/psd-premium/mockup-camiseta-hombre-psd-ropa-logo-verano_53876-141828.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/hombre-afroamericano-camiseta-negra-al-aire-libre_53876-97162.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/hombre-afroamericano-camiseta-negra-al-aire-libre_53876-97162.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/retrato-adulto-joven-maqueta-sudadera-capucha_23-2149296353.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/jovenes-maqueta-sudadera-capucha_23-2149246223.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/psd-gratis/camiseta-maqueta-diseno_15879-478.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/concepto-maqueta-camisa-ropa-sencilla_23-2149448791.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/mujer-camisa-blanca-pantalon-espacio-diseno-ropa-casual-moda-f_53876-125227.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/psd-gratis/chico-joven-posando-camiseta-colorida-gorra_23-2147653516.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/vector-gratis/sudaderas-capucha-realistas-maquetas-sudaderas-capucha-color-verde-ilustracion-vectorial-aislada_1284-80112.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph',
+    'https://img.freepik.com/foto-gratis/feliz-hermosa-joven-ropa-deportiva-pie-calle-sosteniendo-cafe-moda-femenina-estilo-vida-ciudad_132075-9166.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=sph'
+  ]
+
+  mockupData.forEach(async (el) => {
+    await prisma.product.create({
+      data: {
+        name: 'Black t-shirt',
+        description: 'Elevate your wardrobe with our Black T-Shirt collection. Timeless, versatile, and effortlessly stylish. Perfect for any occasion.',
+        image_url: el,
+        price: 14999,
+        brand: {
+          create: {
+            name: 'Starlight',
+            logo_url: 'https://img.freepik.com/vector-gratis/plantilla-logotipo-empresa-moda-diseno-marca-vector-blanco-negro_53876-156444.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=ais',
+          },
+        },
+      },
+    })
+
+
+
+  })
+
+
+
+
+  const product6 = await prisma.product.create({
     data: {
-      name: 'Product Name',
-      description: 'Product Description',
-      image_url: 'https://example.com/image.jpg',
-      price: 1000, // Cambia el precio a lo que desees
+      name: 'Black t-shirt',
+      description: 'Elevate your wardrobe with our Black T-Shirt collection. Timeless, versatile, and effortlessly stylish. Perfect for any occasion.',
+      image_url: 'https://img.freepik.com/foto-gratis/hombre-afroamericano-camiseta-negra-al-aire-libre_53876-97162.jpg?size=626&ext=jpg&ga=GA1.2.1442629563.1692141647&semt=sph',
+      price: 14999,
       brand: {
         create: {
-          name: 'Brand Name',
-          logo_url: 'https://example.com/logo.jpg',
+          name: 'Starlight',
+          logo_url: 'https://img.freepik.com/vector-gratis/plantilla-logotipo-empresa-moda-diseno-marca-vector-blanco-negro_53876-156444.jpg?size=626&ext=jpg&ga=GA1.1.1442629563.1692141647&semt=ais',
         },
       },
     },
   })
 
-  console.log({ product })
+
+
+
+
+
 }
 
 main()
