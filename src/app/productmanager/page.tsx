f7f98697ab7dc32bbca3af88216d869647b3e126
German <german.alejandrozulet@gmail.com>
2023-09-10 01:35:23 -0300

    Added icons for product manager

--- src/app/productmanager/page.tsx
+++ src/app/productmanager/page.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client"
 
 const productmanager = () => {
   return (
-    <div>productmanager </div>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap flex-col">
+        <div className="flex mx-auto flex-wrap mb-20">
+          <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium bg-gray-100 inline-flex items-center leading-none border-indigo-500 text-indigo-500 tracking-wider rounded-t">
+            <img className="w-10 h-10 mr-2" src="/create.svg" alt="createIcon" />
+            Create
+          </a>
+          <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+            <img className="w-10 h-10 mr-2" src="/discard.svg" alt="discardIcon" />
+            DISCARD
+          </a>
+          <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+
+            <img className="w-8 h-8 mr-2" src="/edit.svg" alt="editIcon" />
+            EDIT
+          </a>
+          <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+
+            <img className="w-10 h-10 mr-2" src="/get.svg" alt="getIcon" />
+            GET
+          </a>
+        </div>
+        <img className="xl:w-1/4 lg:w-1/3 md:w-1/2 w-2/3 block mx-auto mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+        <div className="flex flex-col text-center w-full">
+          <h1 className="text-xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
